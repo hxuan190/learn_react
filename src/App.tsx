881972f794195947d4156882d55e3dde00e30f8a
Mai Hoang Xuan <104975107+hxuan190@users.noreply.github.com>
2024-09-15 22:06:20 +0700

    Transforming an array

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,57 @@
 import { useState } from 'react';
 
-
-interface Artist {
+interface Shape {
   id: number;
-  name: string;
+  type: 'circle' | 'square';
+  x: number;
+  y: number;
 }
 
-
-let initialArtists  = [
-  { id: 0, name: 'Marta Colvin Andrade' },
-  { id: 1, name: 'Lamidi Olonade Fakeye'},
-  { id: 2, name: 'Louise Nevelson'},
+let initialShapes: Shape[] = [
+  { id: 0, type: 'circle', x: 50, y: 100 },
+  { id: 1, type: 'square', x: 150, y: 100 },
+  { id: 2, type: 'circle', x: 250, y: 100 },
 ];
 
-let nextId = 0;
+export default function ShapeEditor() {
+  const [shapes, setShapes] = useState<Shape[]>(initialShapes);
 
-export default function List() {
-  const [artists, setArtists] = useState<Artist[]>([]);
+  function handleClick() {
+    const nextShapes = shapes.map(shape => {
+      if (shape.type === 'square') {
+        // No change
+        return shape;
+      } else {
+        // Return a new circle 50px below
+        return {
+          ...shape,
+          y: shape.y + 50,
+        };
+      }
+    });
+    // Re-render with the new array
+    setShapes(nextShapes);
+  }
 
   return (
     <>
-      <h1>Inspiring sculptors:</h1>
-      {artists.map((artist) => {
-        return <div key={artist.id}>{artist.name}{' '} <button onClick={() => {
-          setArtists(artists.filter(a => a.id !== artist.id));
-        }}></button></div>;
-      })}
+      <button onClick={handleClick}>
+        Move circles down!
+      </button>
+      {shapes.map(shape => (
+        <div
+          key={shape.id}
+          style={{
+            background: 'purple',
+            position: 'absolute',
+            left: shape.x,
+            top: shape.y,
+            borderRadius: shape.type === 'circle' ? '50%' : '',
+            width: 20,
+            height: 20,
+          }}
+        />
+      ))}
     </>
   );
 }
