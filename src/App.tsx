92707e8f4b245ca6b0b56e4f312a207da19c78a0
Mai Hoang Xuan <104975107+hxuan190@users.noreply.github.com>
2024-09-18 22:04:45 +0700

    Accessing another component’s DOM nodes

--- src/App.tsx
+++ src/App.tsx
@@ -1,78 +1,22 @@
 import { useRef } from 'react';
 
-export default function CatFriends() {
-  const firstCatRef = useRef<HTMLDivElement>(null);
-  const secondCatRef = useRef<HTMLDivElement>(null);
-  const thirdCatRef = useRef<HTMLDivElement>(null);
+function MyInput(props) {
+  return <input {...props} />;
+}
 
-  function handleScrollToFirstCat() {
-    if (firstCatRef.current) {
-      firstCatRef.current.scrollIntoView({
-        behavior: 'smooth',
-        block: 'nearest',
-        inline: 'center'
-      });
-    }
-  }
+export default function MyForm() {
+  const inputRef = useRef(null);
 
-  function handleScrollToSecondCat() {
-    if (secondCatRef.current) {
-      secondCatRef.current.scrollIntoView({
-        behavior: 'smooth',
-        block: 'nearest',
-        inline: 'center'
-      });
-    }
-  }
-
-  function handleScrollToThirdCat() {
-    if (thirdCatRef.current) {
-      thirdCatRef.current.scrollIntoView({
-        behavior: 'smooth',
-        block: 'nearest',
-        inline: 'center'
-      });
-    }
+  function handleClick() {
+    inputRef.current.focus();
   }
 
   return (
     <>
-      <nav>
-        <button onClick={handleScrollToFirstCat}>
-          Tom
-        </button>
-        <button onClick={handleScrollToSecondCat}>
-          Maru
-        </button>
-        <button onClick={handleScrollToThirdCat}>
-          Jellylorum
-        </button>
-      </nav>
-      <div>
-        <ul>
-          <li>
-            <img
-              src="https://placekitten.com/g/200/200"
-              alt="Tom"
-              ref={firstCatRef}
-            />
-          </li>
-          <li>
-            <img
-              src="https://placekitten.com/g/300/200"
-              alt="Maru"
-              ref={secondCatRef}
-            />
-          </li>
-          <li>
-            <img
-              src="https://placekitten.com/g/250/200"
-              alt="Jellylorum"
-              ref={thirdCatRef}
-            />
-          </li>
-        </ul>
-      </div>
+      <MyInput ref={inputRef} />
+      <button onClick={handleClick}>
+        Focus the input
+      </button>
     </>
   );
 }
