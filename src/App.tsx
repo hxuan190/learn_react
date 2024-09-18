e6f8ffab5200fc85c113d53d14c98a52c18ad4f8
Mai Hoang Xuan <104975107+hxuan190@users.noreply.github.com>
2024-09-18 22:04:11 +0700

    Scrolling to an elementScrolling to an element

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,78 @@
 import { useRef } from 'react';
 
-export default function Counter() {
-  let ref = useRef(0);
+export default function CatFriends() {
+  const firstCatRef = useRef<HTMLDivElement>(null);
+  const secondCatRef = useRef<HTMLDivElement>(null);
+  const thirdCatRef = useRef<HTMLDivElement>(null);
 
-  function handleClick() {
-    ref.current = ref.current + 1;
-    alert('You clicked ' + ref.current + ' times!');
+  function handleScrollToFirstCat() {
+    if (firstCatRef.current) {
+      firstCatRef.current.scrollIntoView({
+        behavior: 'smooth',
+        block: 'nearest',
+        inline: 'center'
+      });
+    }
+  }
+
+  function handleScrollToSecondCat() {
+    if (secondCatRef.current) {
+      secondCatRef.current.scrollIntoView({
+        behavior: 'smooth',
+        block: 'nearest',
+        inline: 'center'
+      });
+    }
+  }
+
+  function handleScrollToThirdCat() {
+    if (thirdCatRef.current) {
+      thirdCatRef.current.scrollIntoView({
+        behavior: 'smooth',
+        block: 'nearest',
+        inline: 'center'
+      });
+    }
   }
 
   return (
-    <button onClick={handleClick}>
-      Click me!
-    </button>
+    <>
+      <nav>
+        <button onClick={handleScrollToFirstCat}>
+          Tom
+        </button>
+        <button onClick={handleScrollToSecondCat}>
+          Maru
+        </button>
+        <button onClick={handleScrollToThirdCat}>
+          Jellylorum
+        </button>
+      </nav>
+      <div>
+        <ul>
+          <li>
+            <img
+              src="https://placekitten.com/g/200/200"
+              alt="Tom"
+              ref={firstCatRef}
+            />
+          </li>
+          <li>
+            <img
+              src="https://placekitten.com/g/300/200"
+              alt="Maru"
+              ref={secondCatRef}
+            />
+          </li>
+          <li>
+            <img
+              src="https://placekitten.com/g/250/200"
+              alt="Jellylorum"
+              ref={thirdCatRef}
+            />
+          </li>
+        </ul>
+      </div>
+    </>
   );
 }
