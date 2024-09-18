fab57625b26fb64df2f49ff4990710d5e7a2258d
Mai Hoang Xuan <104975107+hxuan190@users.noreply.github.com>
2024-09-18 22:01:02 +0700

    Adding a ref to your component

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,16 @@
-import { useState } from 'react';
+import { useRef } from 'react';
 
-interface Shape {
-  id: number;
-  type: 'circle' | 'square';
-  x: number;
-  y: number;
-}
-
-let initialShapes: Shape[] = [
-  { id: 0, type: 'circle', x: 50, y: 100 },
-  { id: 1, type: 'square', x: 150, y: 100 },
-  { id: 2, type: 'circle', x: 250, y: 100 },
-];
-
-export default function ShapeEditor() {
-  const [shapes, setShapes] = useState<Shape[]>(initialShapes);
+export default function Counter() {
+  let ref = useRef(0);
 
   function handleClick() {
-    const nextShapes = shapes.map(shape => {
-      if (shape.type === 'square') {
-        // No change
-        return shape;
-      } else {
-        // Return a new circle 50px below
-        return {
-          ...shape,
-          y: shape.y + 50,
-        };
-      }
-    });
-    // Re-render with the new array
-    setShapes(nextShapes);
+    ref.current = ref.current + 1;
+    alert('You clicked ' + ref.current + ' times!');
   }
 
   return (
-    <>
-      <button onClick={handleClick}>
-        Move circles down!
-      </button>
-      {shapes.map(shape => (
-        <div
-          key={shape.id}
-          style={{
-            background: 'purple',
-            position: 'absolute',
-            left: shape.x,
-            top: shape.y,
-            borderRadius: shape.type === 'circle' ? '50%' : '',
-            width: 20,
-            height: 20,
-          }}
-        />
-      ))}
-    </>
+    <button onClick={handleClick}>
+      Click me!
+    </button>
   );
 }
