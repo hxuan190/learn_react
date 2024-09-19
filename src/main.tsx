7c1fd3d109818053babd813669943c131db41dda
Mai Hoang Xuan <104975107+hxuan190@users.noreply.github.com>
2024-09-19 23:56:01 +0700

    an alternative to passing props

--- src/main.tsx
+++ src/main.tsx
@@ -1,76 +1,15 @@
-import { useState, useContext } from 'react';
-import { TasksContext, TasksDispatchContext } from './TasksContext.js';
+import Heading from './Heading.js';
+import Section from './Section.js';
 
-export default function TaskList() {
-  const tasks = useContext(TasksContext);
+export default function Page() {
   return (
-    <ul>
-      {tasks.map(task => (
-        <li key={task.id}>
-          <Task task={task} />
-        </li>
-      ))}
-    </ul>
-  );
-}
-
-function Task({ task }) {
-  const [isEditing, setIsEditing] = useState(false);
-  const dispatch = useContext(TasksDispatchContext);
-  let taskContent;
-  if (isEditing) {
-    taskContent = (
-      <>
-        <input
-          value={task.text}
-          onChange={e => {
-            dispatch({
-              type: 'changed',
-              task: {
-                ...task,
-                text: e.target.value
-              }
-            });
-          }} />
-        <button onClick={() => setIsEditing(false)}>
-          Save
-        </button>
-      </>
-    );
-  } else {
-    taskContent = (
-      <>
-        {task.text}
-        <button onClick={() => setIsEditing(true)}>
-          Edit
-        </button>
-      </>
-    );
-  }
-  return (
-    <label>
-      <input
-        type="checkbox"
-        checked={task.done}
-        onChange={e => {
-          dispatch({
-            type: 'changed',
-            task: {
-              ...task,
-              done: e.target.checked
-            }
-          });
-        }}
-      />
-      {taskContent}
-      <button onClick={() => {
-        dispatch({
-          type: 'deleted',
-          id: task.id
-        });
-      }}>
-        Delete
-      </button>
-    </label>
+    <Section>
+      <Heading level={1}>Title</Heading>
+      <Heading level={2}>Heading</Heading>
+      <Heading level={3}>Sub-heading</Heading>
+      <Heading level={4}>Sub-sub-heading</Heading>
+      <Heading level={5}>Sub-sub-sub-heading</Heading>
+      <Heading level={6}>Sub-sub-sub-sub-heading</Heading>
+    </Section>
   );
 }
